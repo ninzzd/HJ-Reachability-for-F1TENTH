f7185b2a3a43ba1e61c64f39d74052cb0d389ca0
ninzzd <ninaad.desai1212@gmail.com>
2025-06-02 09:33:06 +0000

    Removing the optimized_dp package for now Will work on self-implementation and then verify the results with other packages like helperOC, BEACLS, etc

--- optimized_dp/resource/optimized_dp/odp/Plots/__init__.py
+++
@@ -1,4 +0,0 @@
-from odp.Plots.plotting_utilities import plot_isosurface
-from odp.Plots.plotting_utilities import plot_valuefunction
-from odp.Plots.plotting_utilities import downsample
-from odp.Plots.plot_options import PlotOptions
