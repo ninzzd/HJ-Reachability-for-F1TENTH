2a14ee0b876c62286defc9840daecba1609b9885
ninzzd <ninaad.desai1212@gmail.com>
2025-06-04 18:34:14 +0000

    SDF Visualization with visualization_msgs/Marker successful Stops updating after working properly for some duration

--- rt_reachability/src/sdf_node.cpp
+++ rt_reachability/src/sdf_node.cpp
@@ -1,5 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 #include "rt_reachability/Grid.hpp"
 #include "rt_reachability/SDF.hpp"
 #include <cmath>
@@ -9,13 +11,19 @@ using namespace rt_reachability;
 class SDFNode : public rclcpp::Node {
   public:
     int count;
+    int sent;
+    visualization_msgs::msg::Marker prev_marker;
     SDFNode() : Node("sdf") {
       this->count = 0;
+      this->sent = 0;
       auto default_qos = rclcpp::QoS(rclcpp::SystemDefaultsQoS());
 
       subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
           "/scan", default_qos,
           std::bind(&SDFNode::topic_callback, this, _1));
+      publisher_ = this->create_publisher<visualization_msgs::msg::Marker>(
+          "/sdf_grid_markers",10
+      );
     }
 
   private:
@@ -30,12 +38,12 @@ class SDFNode : public rclcpp::Node {
       // std::cout << "No. of Angles Stored: " << (int) n_obstacles << std::endl;
       // std::cout << "Length of ranges[]: " <<  << std::endl;
       float* r_obstacles = (float*) malloc(sizeof(float) * num_obstacles);
+      float* valuefunc2D = (float*) malloc(sizeof(float) * Grid::getSizeX() * Grid::getSizeY());
       for(int i = 0; i < (int)num_obstacles; i++){
         r_obstacles[i] = (float)msg->ranges[i];
         // std::cout << r_obstacles[i] << std::endl;
       }
       // ---Obstacle Set Identification---
-      float* valuefunc2D = (float*) malloc(sizeof(float) * Grid::getSizeX() * Grid::getSizeY());
       // rt_reachability::computeObstacleSet(r_obstacles,(int)num_obstacles,(float)msg->angle_min,(float)msg->angle_max,(float)msg->angle_increment,valuefunc2D);
       
       // ---Primitive Grid (Obstacle-Set Only) Visualization in Terminal---
@@ -52,9 +60,11 @@ class SDFNode : public rclcpp::Node {
       // }
       // std::cout << std::endl;
 
-      // ---Cylindrical-To-Cartesian---
+      // --- Cylindrical-To-Cartesian ---
       // float* x_obstacles = (float*) malloc(sizeof(float) * num_obstacles);
       // float* y_obstacles = (float*) malloc(sizeof(float) * num_obstacles);
+
+      // --- Terminal Display of Cartesian Coordinates and Computed Distance ---
       // rt_reachability::cylToCart(r_obstacles,(int)num_obstacles,(float)msg->angle_min,(float)msg->angle_max,(float)msg->angle_increment,x_obstacles,y_obstacles);
       // for(int i = 0;i < (int)num_obstacles;i++){
       //   float exp = r_obstacles[i];
@@ -62,19 +72,57 @@ class SDFNode : public rclcpp::Node {
       //   std::cout << "Cartesian Coordinates of the Obstacles: (" << x_obstacles[i] << "," << y_obstacles[i] << "); Expected distance: " << exp << "; Calculated distance: " <<  calc << " ; Error: " << exp - calc << std::endl;
       // }
 
-      // ---SDF Computation---
+      // --- SDF Computation ---
       rt_reachability::computeSDF(r_obstacles,num_obstacles,(float)msg->angle_min,(float)msg->angle_max,(float)msg->angle_increment,valuefunc2D);
-      std::cout << std::endl;
+
+      // --- Terminal Visualization of SDF ---
+      // std::cout << std::endl;
+      // for(int i = 0;i < Grid::getSizeX();i++){
+      //   for(int j = 0;j < Grid::getSizeY();j++){
+      //     std::cout << valuefunc2D[i*Grid::getSizeY() + j] << " ";
+      //   }
+      //   std::cout << std::endl;
+      // }
+      // std::cout << std::endl;
+
+      // --- Marker Visualization of SDF in RViz ---
+      // if(this->sent == 1){
+      //   prev_marker.action = visualization_msgs::msg::Marker::DELETE;
+      //   publisher_->publish(prev_marker);
+      // }
+      visualization_msgs::msg::Marker marker = visualization_msgs::msg::Marker();
+      marker.header.stamp = rclcpp::Time(0);
+      marker.header.frame_id = "ego_racecar/base_link";
+      marker.id = 1;
+      marker.ns = "SDF Grid";
+      marker.type = visualization_msgs::msg::Marker::POINTS;
+      marker.action = visualization_msgs::msg::Marker::ADD;
+      marker.scale.x = 0.05;
+      marker.scale.y = 0.05;
+      marker.scale.z = 0.05;
+      marker.color.r = 1.0f;
+      marker.color.g = 0.0f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0f;
       for(int i = 0;i < Grid::getSizeX();i++){
         for(int j = 0;j < Grid::getSizeY();j++){
-          std::cout << valuefunc2D[i*Grid::getSizeY() + j] << " ";
+          geometry_msgs::msg::Point point;
+          point.x = Grid::getMaxX() - i*(float)(Grid::getMaxX() - Grid::getMinX())/(Grid::getSizeX()-1);
+          point.y = Grid::getMaxY() - j*(float)(Grid::getMaxY() - Grid::getMinY())/(Grid::getSizeY()-1);
+          point.z = valuefunc2D[i*Grid::getSizeY() + j];
+          marker.points.push_back(point);
         }
-        std::cout << std::endl;
       }
-      std::cout << std::endl;
-      this->count++;
+      marker.lifetime = rclcpp::Duration::from_seconds(1);
+      publisher_->publish(marker);
+      // prev_marker = marker;
+      // if(this->sent == 0 ) this->sent = 1;
+      free(r_obstacles);
+      free(valuefunc2D);
+      // this->count++;
     }
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr publisher_;
   };
 
   int main(int argc, char *argv[]) {
@@ -88,11 +136,11 @@ class SDFNode : public rclcpp::Node {
     // The first cudaMalloc command takes almost a tenth of a second to execute
     // firstInitMem() takes the brunt of the slow cudaMalloc command as it allocates and frees memory to a dummy pointer
     firstInitMem();
-    // rclcpp::spin(node);
-    while(rclcpp::ok() && node->count < 1){
-      rclcpp::spin_some(node);
-    }
-    if(rclcpp::ok())
-      rclcpp::shutdown();
+    rclcpp::spin(node);
+    // while(rclcpp::ok() && node->count < 1){
+    //   rclcpp::spin_some(node);
+    // }
+    // if(rclcpp::ok())
+    rclcpp::shutdown();
     return 0;
 }
