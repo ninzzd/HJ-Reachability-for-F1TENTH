d489921117ed46d63b6e5d10da83d0db7b1e0aa8
ninzzd <ninaad.desai1212@gmail.com>
2025-06-05 18:40:50 +0000

    Successful Computation of the Local Signed Distance Field using GPU-Acceleration Made an cuda error handling function for debugging Fixed the previous error by calling the cudaStreamDestroy() and cudaEventDestory() function, which were missing Added a screen recording of the SDF visualization while teleoperating the car

--- rt_reachability/src/SDF.cu
+++ rt_reachability/src/SDF.cu
@@ -107,6 +107,14 @@ void rt_reachability::cylToCart(float* r_obstacles,int num_obstacles,float angle
     cudaFree(cuda_x_obstacles);
     cudaFree(cuda_y_obstacles);
 }
+void checkCUDAerror(cudaError_t err,const char* msg = nullptr){
+    if(err != cudaSuccess){
+        std::cout << "CUDA Error: "<< cudaGetErrorString(err);
+        if(msg) std::cout << ": " << msg << std::endl;
+        else std::cout << std::endl;
+        throw std::runtime_error("Terminating SDF Computation\n");
+    }
+}
 void rt_reachability::computeSDF(float* r_obstacles,int num_obstacles,float angle_min,float angle_max,float angle_inc,float* value_func){
     float* cuda_r_obstacles;
     float* cuda_value_function;
@@ -114,33 +122,57 @@ void rt_reachability::computeSDF(float* r_obstacles,int num_obstacles,float angl
     float* cuda_y_obstacles;
 
     cudaStream_t obstacleSet, cylToCart;
-    cudaStreamCreate(&obstacleSet);
-    cudaStreamCreate(&cylToCart);
+    cudaEvent_t event1, event2;
+    checkCUDAerror(cudaStreamCreate(&obstacleSet),"Error in Obstacle Set Kernel Stream Creation");
+    checkCUDAerror(cudaStreamCreate(&cylToCart),"Error in Cylindrical-to-Cartesian Kernel Stream Creation");
+    checkCUDAerror(cudaEventCreate(&event1),"Error in Obstacle Set Kernel Event Creation");
+    checkCUDAerror(cudaEventCreate(&event2),"Error in Cylindical-To-Cartesian Kernel Event Creation");
 
-    cudaMalloc(&cuda_r_obstacles,sizeof(float)*num_obstacles);
-    cudaMalloc(&cuda_value_function,sizeof(float)*rt_reachability::Grid::getSizeX()*rt_reachability::Grid::getSizeY());
-    cudaMalloc(&cuda_x_obstacles,sizeof(float)*num_obstacles);
-    cudaMalloc(&cuda_y_obstacles,sizeof(float)*num_obstacles);
-    cudaMemcpy(cuda_r_obstacles,r_obstacles,sizeof(float)*num_obstacles,cudaMemcpyHostToDevice);
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (Before Attempting to Allocate Memory) ");
+    checkCUDAerror(cudaMalloc(&cuda_r_obstacles,sizeof(float)*num_obstacles),"Error in Memory Allocation to cuda_r_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Allocating Memory to cuda_r_obstacles) ");
+    checkCUDAerror(cudaMalloc(&cuda_value_function,sizeof(float)*rt_reachability::Grid::getSizeX()*rt_reachability::Grid::getSizeY()),"Error in Memory Allocation to cuda_value_function");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Allocating Memory to cuda_value_function) ");
+    checkCUDAerror(cudaMalloc(&cuda_x_obstacles,sizeof(float)*num_obstacles),"Error in Memory Allocation to cuda_x_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Allocating Memory to cuda_x_obstacles) ");
+    checkCUDAerror(cudaMalloc(&cuda_y_obstacles,sizeof(float)*num_obstacles),"Error in Memory Allocation to cuda_y_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Allocating Memory to cuda_y_obstacles) ");
+    checkCUDAerror(cudaMemcpy(cuda_r_obstacles,r_obstacles,sizeof(float)*num_obstacles,cudaMemcpyHostToDevice),"Error in Memory Copying from r_obstacles to cuda_r_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Copying Memory into cuda_r_obstacles) ");
 
     // dim3 blockSize(32*n);
     // dim3 gridSize(1+(int)((Grid::getSizeX()*Grid::getSizeY())/(32*n)));
     auto start = std::chrono::high_resolution_clock::now();
     computeObstacleSetKernel<<<dim3(50),dim3(50),0,obstacleSet>>>(cuda_r_obstacles,num_obstacles,angle_min,angle_max,angle_inc,cuda_value_function,rt_reachability::Grid::getMaxX(),rt_reachability::Grid::getMinX(),rt_reachability::Grid::getSizeX(),rt_reachability::Grid::getMaxY(),rt_reachability::Grid::getMinY(),rt_reachability::Grid::getSizeY());
+    checkCUDAerror(cudaEventRecord(event1,obstacleSet),"Error in Recording the Event for Obstacle Set Kernel");
     cylToCartKernel<<<dim3(2),dim3(1024),0,cylToCart>>>(cuda_r_obstacles, num_obstacles, angle_min, angle_max, angle_inc, cuda_x_obstacles, cuda_y_obstacles);
+    checkCUDAerror(cudaEventRecord(event2,cylToCart),"Error in Recording the Event for Cylindircal-to-Cartesian Kernel");
+    checkCUDAerror(cudaStreamWaitEvent(0,event1,0),"Error in Waiting for End of Execution of Obstacle Set Kernel");
+    checkCUDAerror(cudaStreamWaitEvent(0,event2,0),"Error in Waiting for End of Execution of Cylindrical-to-Cartesian Kernel");
     auto end =   std::chrono::high_resolution_clock::now();
     auto stream_duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-    std::cout << "Simulatneous Obstacle Set and Cartesian Coordinates Processing Time: " << stream_duration << " microseconds" << std::endl;
-
+    std::cout << "\033[2A";
+    std::cout << "\rSimulatneous Obstacle Set and Cartesian Coordinates Processing Time (in microseconds) : " << stream_duration << std::endl;
+    checkCUDAerror(cudaStreamDestroy(obstacleSet),"Error in Destroying the Obstacle Set Kernel Stream");
+    checkCUDAerror(cudaStreamDestroy(cylToCart),"Error in Destroying the Cylindrical-to-Cartesian Kernel Stream");
+    checkCUDAerror(cudaEventDestroy(event1),"Error in Destroying the Obstacle Set Kernel Event");
+    checkCUDAerror(cudaEventDestroy(event2),"Error in Destroying the Cylindrical-to-Cartesian Kernel Event");
     start = std::chrono::high_resolution_clock::now();
     computeSDFKernel<<<dim3(50),dim3(50)>>>(cuda_x_obstacles, cuda_y_obstacles,num_obstacles,cuda_value_function,rt_reachability::Grid::getMaxX(),rt_reachability::Grid::getMinX(),rt_reachability::Grid::getSizeX(),rt_reachability::Grid::getMaxY(),rt_reachability::Grid::getMinY(),rt_reachability::Grid::getSizeY());
     end =   std::chrono::high_resolution_clock::now();
     auto sdf_duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-    std::cout << "SDF Processing Time: " << sdf_duration << " microseconds" << std::endl;
-
-    cudaMemcpy(value_func,cuda_value_function,sizeof(float)*rt_reachability::Grid::getSizeX()*rt_reachability::Grid::getSizeY(),cudaMemcpyDeviceToHost);
-    cudaFree(cuda_x_obstacles);
-    cudaFree(cuda_y_obstacles);
-    cudaFree(cuda_r_obstacles);
-    cudaFree(cuda_value_function);
+    std::cout << "\rSDF Processing Time (in microseconds): " << sdf_duration << std::endl;
+    
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization Before Final Memory Copying (Before Attempting to Freeing Memory) ");
+    checkCUDAerror(cudaMemcpy(value_func,cuda_value_function,sizeof(float)*rt_reachability::Grid::getSizeX()*rt_reachability::Grid::getSizeY(),cudaMemcpyDeviceToHost),"Error in Memory Copying from cuda_value_function to value_function");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Copying Memory into value_function) ");
+    checkCUDAerror(cudaFree(cuda_r_obstacles),"Error in Freeing Memory for cuda_r_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Freeing Memory of cuda_r_obstacles) ");
+    checkCUDAerror(cudaFree(cuda_value_function),"Error in Freeing Memory for cuda_value_function");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Freeing Memory of cuda_value_function) ");
+    checkCUDAerror(cudaFree(cuda_x_obstacles),"Error in Freeing Memory for cuda_x_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Freeing Memory of cuda_x_obstacles) ");
+    checkCUDAerror(cudaFree(cuda_y_obstacles),"Error in Freeing Memory for cuda_y_obstacles");
+    checkCUDAerror(cudaDeviceSynchronize(),"Error in Device Synchronization (After Freeing Memory of cuda_y_obstacles) ");
+    
 }
--- rt_reachability/src/sdf_node.cpp
+++ rt_reachability/src/sdf_node.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 #include "geometry_msgs/msg/point.hpp"
 #include "visualization_msgs/msg/marker.hpp"
 #include "rt_reachability/Grid.hpp"
@@ -21,8 +22,8 @@ class SDFNode : public rclcpp::Node {
       subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
           "/scan", default_qos,
           std::bind(&SDFNode::topic_callback, this, _1));
-      publisher_ = this->create_publisher<visualization_msgs::msg::Marker>(
-          "/sdf_grid_markers",10
+      publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+          "/sdf_grid_pointcloud",10
       );
     }
 
@@ -76,6 +77,7 @@ class SDFNode : public rclcpp::Node {
       rt_reachability::computeSDF(r_obstacles,num_obstacles,(float)msg->angle_min,(float)msg->angle_max,(float)msg->angle_increment,valuefunc2D);
 
       // --- Terminal Visualization of SDF ---
+
       // std::cout << std::endl;
       // for(int i = 0;i < Grid::getSizeX();i++){
       //   for(int j = 0;j < Grid::getSizeY();j++){
@@ -90,39 +92,73 @@ class SDFNode : public rclcpp::Node {
       //   prev_marker.action = visualization_msgs::msg::Marker::DELETE;
       //   publisher_->publish(prev_marker);
       // }
-      visualization_msgs::msg::Marker marker = visualization_msgs::msg::Marker();
-      marker.header.stamp = rclcpp::Time(0);
-      marker.header.frame_id = "ego_racecar/base_link";
-      marker.id = 1;
-      marker.ns = "SDF Grid";
-      marker.type = visualization_msgs::msg::Marker::POINTS;
-      marker.action = visualization_msgs::msg::Marker::ADD;
-      marker.scale.x = 0.05;
-      marker.scale.y = 0.05;
-      marker.scale.z = 0.05;
-      marker.color.r = 1.0f;
-      marker.color.g = 0.0f;
-      marker.color.b = 0.0f;
-      marker.color.a = 1.0f;
-      for(int i = 0;i < Grid::getSizeX();i++){
-        for(int j = 0;j < Grid::getSizeY();j++){
-          geometry_msgs::msg::Point point;
-          point.x = Grid::getMaxX() - i*(float)(Grid::getMaxX() - Grid::getMinX())/(Grid::getSizeX()-1);
-          point.y = Grid::getMaxY() - j*(float)(Grid::getMaxY() - Grid::getMinY())/(Grid::getSizeY()-1);
-          point.z = valuefunc2D[i*Grid::getSizeY() + j];
-          marker.points.push_back(point);
-        }
-      }
-      marker.lifetime = rclcpp::Duration::from_seconds(1);
-      publisher_->publish(marker);
+
+      // --- Marker Visualization ---
+
+      // visualization_msgs::msg::Marker marker = visualization_msgs::msg::Marker();
+      // marker.header.stamp = rclcpp::Time(0);
+      // marker.header.frame_id = "ego_racecar/base_link";
+      // marker.id = 1;
+      // marker.ns = "SDF Grid";
+      // marker.type = visualization_msgs::msg::Marker::POINTS;
+      // marker.action = visualization_msgs::msg::Marker::ADD;
+      // marker.scale.x = 0.05;
+      // marker.scale.y = 0.05;
+      // marker.scale.z = 0.05;
+      // marker.color.r = 1.0f;
+      // marker.color.g = 0.0f;
+      // marker.color.b = 0.0f;
+      // marker.color.a = 1.0f;
+      // for(int i = 0;i < Grid::getSizeX();i++){
+      //   for(int j = 0;j < Grid::getSizeY();j++){
+      //     geometry_msgs::msg::Point point;
+      //     point.x = Grid::getMaxX() - i*(float)(Grid::getMaxX() - Grid::getMinX())/(Grid::getSizeX()-1);
+      //     point.y = Grid::getMaxY() - j*(float)(Grid::getMaxY() - Grid::getMinY())/(Grid::getSizeY()-1);
+      //     point.z = valuefunc2D[i*Grid::getSizeY() + j];
+      //     marker.points.push_back(point);
+      //   }
+      // }
+      // marker.lifetime = rclcpp::Duration::from_seconds(1);
+      // publisher_->publish(marker);
       // prev_marker = marker;
       // if(this->sent == 0 ) this->sent = 1;
+
+      // --- PointCloud Visualization ---
+
+      sensor_msgs::msg::PointCloud2 pc_msg;
+      pc_msg.header.stamp = rclcpp::Time(0);
+      pc_msg.header.frame_id = "ego_racecar/base_link";
+      pc_msg.height = 1;
       free(r_obstacles);
+      pc_msg.width = Grid::getSizeX()*Grid::getSizeY();
+      pc_msg.is_dense = true;
+      sensor_msgs::msg::PointField field;
+      field.name = "x"; field.offset = 0; field.datatype = sensor_msgs::msg::PointField::FLOAT32; field.count = 1;
+      pc_msg.fields.push_back(field);
+      field.name = "y"; field.offset = 4; pc_msg.fields.push_back(field);
+      field.name = "z"; field.offset = 8; pc_msg.fields.push_back(field);
+      pc_msg.point_step = 12; // 3 * 4 bytes (float32)
+      pc_msg.row_step = pc_msg.point_step * pc_msg.width;
+      pc_msg.data.resize(pc_msg.row_step*pc_msg.height);
+      for(int i = 0;i < Grid::getSizeX();i++){
+        for(int j = 0;j < Grid::getSizeY();j++){
+          size_t point_idx = i*Grid::getSizeY() + j;
+          size_t byte_idx = point_idx *pc_msg.point_step;
+          float x = Grid::getMaxX() - i*(Grid::getMaxX() - Grid::getMinX())/(Grid::getSizeX() - 1);
+          float y = Grid::getMaxY() - j*(Grid::getMaxY() - Grid::getMinY())/(Grid::getSizeY() - 1);
+          float z = valuefunc2D[i*Grid::getSizeY() + j];
+          memcpy(&pc_msg.data[byte_idx+0],&x,sizeof(float));
+          memcpy(&pc_msg.data[byte_idx+4],&y,sizeof(float));
+          memcpy(&pc_msg.data[byte_idx+8],&z,sizeof(float));
+        }
+      }
+      publisher_->publish(pc_msg);
+
       free(valuefunc2D);
       // this->count++;
     }
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
   };
 
   int main(int argc, char *argv[]) {
@@ -133,6 +169,9 @@ class SDFNode : public rclcpp::Node {
     Grid::setLowerBounds(0.0,-2.0,0.1,-((float)M_PI)/6);
     Grid::setUpperBounds(4.0,+2.0,5.0,+((float)M_PI)/6);
     
+    // Bufferring the bash display to allows in-place display
+    std::cout << std::endl;
+    std::cout << std::endl;
     // The first cudaMalloc command takes almost a tenth of a second to execute
     // firstInitMem() takes the brunt of the slow cudaMalloc command as it allocates and frees memory to a dummy pointer
     firstInitMem();
